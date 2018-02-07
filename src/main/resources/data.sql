b9d02d8d51be66a406e62e88f14b20d4ff765e3c
Alfredo Daniel Ramos Kustron <alkustron@gmail.com>
2018-02-07 14:47:43 +0100

    Changed data to be loaded into the database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO Stock(name, currentPrice, lastUpdate) VALUES ('payconiq', 5000.75, CURRENT_TIMESTAMP);
+INSERT INTO Stock(name, currentPrice, lastUpdate) VALUES ('facebook', 5000.75, CURRENT_TIMESTAMP);
 INSERT INTO Stock(name, currentPrice, lastUpdate) VALUES ('yahoo', 1500.85, CURRENT_TIMESTAMP);
 INSERT INTO Stock(name, currentPrice, lastUpdate) VALUES ('google', 2000.95, CURRENT_TIMESTAMP);
 INSERT INTO Stock(name, currentPrice, lastUpdate) VALUES ('amazon', 2500.60, CURRENT_TIMESTAMP);
